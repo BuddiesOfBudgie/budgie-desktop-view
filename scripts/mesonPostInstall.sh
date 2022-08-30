b76a518b97beb65d1096344f6ba9e9a3f647d9a1
Joshua Strobl <me@joshuastrobl.com>
2022-08-30 14:30:38 +0300

    Remove unused gtk-update-icon-cache

--- scripts/mesonPostInstall.sh
+++ scripts/mesonPostInstall.sh
@@ -10,7 +10,5 @@ if [ -z $DESTDIR ]; then
 	glib-compile-schemas "$PREFIX/share/glib-2.0/schemas"
 	echo 'Updating desktop database'
 	update-desktop-database -q
-	echo 'Updating icon cache'
-	gtk-update-icon-cache -q -t -f "$PREFIX/share/icons/hicolor"
 
 fi
